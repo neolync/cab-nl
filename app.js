7dc18a87faf38db40072135799875a59223cb4dd
smbasha-nl <sm.basha@neolync.com>
2024-12-28 15:07:55 +0530

    new1

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 // Function to save data
